ed7b8dccb0822a3e2f2931f78c017fa1622b4002
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-11 13:14:49 -0300

    modify postion <a> and modify dom navbar-mobile

--- views/script/index.js
+++ views/script/index.js
@@ -1,11 +1,14 @@
-const navbarIcon = document.querySelector(".icon");
-const navbarMenuMobile = document.querySelector(".navbar-mobile");
-
+const navbarIcon = document.querySelector("header > a");
+const navbarMenuMobile = document.querySelector("header").lastElementChild;
 
 function toggleNavbarMenuMobile(){
-    navbarMenuMobile.classList.toggle("show");
-    navbarMenuMobile.classList.toggle("height");
-}
+    console.log(navbarMenuMobile.classList.length)
+    if(navbarMenuMobile.classList.length === 1){
+        navbarMenuMobile.setAttribute("class", "navbar-mobile show height");
+    }else{
+        navbarMenuMobile.setAttribute("class", "navbar-mobile");
+    }
 
+}
 navbarIcon.addEventListener("click", toggleNavbarMenuMobile)
 
