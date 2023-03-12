eaf95a04afd80b7a550b20658c8682c48c300e4a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-11 23:32:43 -0300

    create functionality for show shadow in imgs

--- views/script/index.js
+++ views/script/index.js
@@ -1,8 +1,13 @@
 const navbarIcon = document.querySelector("header > a");
 const navbarMenuMobile = document.querySelector("header").lastElementChild;
 
+const shadowLikes = document.querySelectorAll("main section div");
+const sections = document.querySelectorAll("section");
+
+const figures = document.querySelectorAll("figure");
+
 function toggleNavbarMenuMobile(){
-    console.log(navbarMenuMobile.classList.length)
+    
     if(navbarMenuMobile.classList.length === 1){
         navbarMenuMobile.setAttribute("class", "navbar-mobile show height");
     }else{
@@ -11,4 +16,34 @@ function toggleNavbarMenuMobile(){
 
 }
 navbarIcon.addEventListener("click", toggleNavbarMenuMobile)
+
+// Efeito que adiciona sombra nas imagens principal
+sections.forEach(section => {
+    section.addEventListener("mouseover", () =>{
+        let sectionId = section.getAttribute("id")
+
+        if(sectionId === "section-1" || sectionId === "section-2"){
+            section.setAttribute("class", "shadowLikeMain");
+        }
+    });
+})
+// Efeito que remove sombra nas imagens principal
+sections.forEach(section => {
+    section.addEventListener("mouseleave", () =>{
+        section.removeAttribute("class", "shadowLike");
+    });
+})
+// Efeito que adiciona sombra nas imagens secundárias
+figures.forEach(figure => {
+    figure.addEventListener("mouseover", () =>{
+        figure.setAttribute("class", "shadowLikeSecondary")
+    })
+})
+figures.forEach(figure => {
+    figure.addEventListener("mouseleave", () =>{
+        figure.removeAttribute("class", "shadowLikeSecondary")
+    })
+})
+
+
 
