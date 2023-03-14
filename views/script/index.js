8d555feb0a18c6df898928a5c8c480588c402528
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-14 10:24:53 -0300

    create funct for icone search scroll and modify

--- views/script/index.js
+++ views/script/index.js
@@ -7,20 +7,38 @@ const sections = document.querySelectorAll("section");
 const imagesPost = document.querySelectorAll("figure img");
 const divShadowMainPost = document.querySelectorAll("main > article section figure > div")
 
+const body = document.querySelector("body");
+
+const navItemSearch = document.querySelector("header nav").lastElementChild;
+
 imagesPost.forEach(image => {
     image.setAttribute("title", "Ler mais")
+})
+
+document.addEventListener("scroll", (event) => {
+     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navItemSearch.setAttribute("id","showIconeSearch")
+     }else{
+        navItemSearch.removeAttribute("id","showIconeSearch")
+     }
 })
 
 
 function toggleNavbarMenuMobile(){
     
     if(navbarMenuMobile.classList.length === 1){
-        navbarMenuMobile.setAttribute("class", "navbar-mobile show height");
+        navbarMenuMobile.classList.add("navbar-mobile show height");
     }else{
-        navbarMenuMobile.setAttribute("class", "navbar-mobile");
+        navbarMenuMobile.classList.remove("navbar-mobile");
     }
 
 }
+
+
+
+
+// Mostar icone para pesquisar ao rolar a barra de scroll
+
 
 // <------- Efeito Show menu de Naegação Mobile ------->
 navbarIcon.addEventListener("click", toggleNavbarMenuMobile)
@@ -29,34 +47,33 @@ navbarIcon.addEventListener("click", toggleNavbarMenuMobile)
 // <------- Efeito Zom e Sombra Article Main------->
 // Efeito que adiciona zom e sombra nas imagens principal
 imagesPost.forEach(image => {
-    image.addEventListener("mouseover", () =>{
-        let divShadow = image.nextElementSibling;
-        image.setAttribute("class", "zomImageArticleMain");
+    image.addEventListener("mouseenter", () =>{
+        image.classList.add("zomImageArticleMain");
     });
 })
 // Efeito que remove zom e sombra nas imagens principal
 imagesPost.forEach(image => {
     image.addEventListener("mouseleave", () =>{
-        image.removeAttribute("class", "zomImageArticleMain");
+        image.classList.remove("zomImageArticleMain");
     });
 })
 
-// <------- Efeito Zom Article Secondary------->
-// Efeito que adiciona sombra nas imagens secundárias
+// <------- Efeito Zom Article------->
+// Efeito que adiciona zom nas imagens 
 imagesPost.forEach(image => {
-    image.addEventListener("mouseover", () =>{
+    image.addEventListener("mouseenter", () =>{
         let imageId = image.getAttribute("id");
         if(imageId !== "image-1" && imageId !== "image-2"){
-            image.setAttribute("class", "zomImageArticleNews")
+            image.classList.add("zomImageArticle")
         }
     })
 })
-// Efeito que remove sombra nas imagens secundárias
+// Efeito que remove zom nas imagens
 imagesPost.forEach(image => {
-    image.addEventListener("mouseleave", () =>{
+    image.addEventListener("mouseenter", () =>{
         let imageId = image.getAttribute("id");
         if(imageId !== "image-1" && imageId !== "image-2"){
-            image.removeAttribute("class", "zomImageArticleNews")
+            image.classList.remove("zomImageArticle")
         }
         
     })
