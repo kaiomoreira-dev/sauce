8c2db284076b2cf266fe07f94066ca999a565994
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-17 09:44:26 -0300

    add engine ejs for look ejs in folder views

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//engine para entender o ejs na pasta views
+app.use("view engine", "ejs");
+
 app.listen(3333, () =>{
     console.log('Server listening on port 3333. . .')
 })
