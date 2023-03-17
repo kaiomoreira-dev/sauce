f3091950b442d670165ef51869be8a2164c6e6eb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-17 10:33:16 -0300

    define path for file css,images and script

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const { request } = require('http');
 
 const app = express();
 
 //engine para entender o ejs na pasta views
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
+app.use(express.static("views/css"));
+app.use(express.static("views/script"));
+app.use(express.static("views/images"));
 
-app.listen(3333, () =>{
-    console.log('Server listening on port 3333. . .')
+
+app.get("/", (request, response) =>{
+    response.render("pages/index.ejs")
+});
+
+app.listen(8080, () =>{
+    console.log('Server listening on port 8080. . .')
 })
 
