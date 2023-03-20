b9250407a50b7a74dcdad2e32207c519b3b0a213
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-20 17:15:24 -0300

    arrange path for routes and create route tambaqui

--- server.js
+++ server.js
@@ -11,18 +11,26 @@ app.get("/", (request, response) =>{
     response.render("pages/index.ejs")
 });
 
-app.get("/meat", (request, response) =>{
-    response.render("pages/meat.ejs")
-});
 
-app.get("/about", (request, response) =>{
+app.get("/sobre", (request, response) =>{
     response.render("pages/about.ejs")
 });
 
-app.get("/contact", (request, response) =>{
+app.get("/contato", (request, response) =>{
     response.render("pages/contact.ejs")
 });
 
+// <---------- Categorias das Receitas ---------->
+app.get("/categoria/carnes", (request, response) =>{
+    response.render("pages/meat.ejs")
+});
+
+// <---------- Carnes ---------->
+app.get("/categoria/carne/tambaqui-assado-no-forno", (request, response) =>{
+    response.render("pages/categories/meats/tambaqui.ejs")
+});
+
+// <---------- Sevidor conectado na porta 8080 ---------->
 app.listen(8080, () =>{
     console.log('Server listening on port 8080. . .')
 })
