efb87d1c3db69fba38bf679e63e1fdafc8b6e3b3
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-20 22:06:53 -0300

    create function for return object recipes

---
+++ views/module/Recipes.js
@@ -0,0 +1,41 @@
+module.exports = function Recipes(){
+    const recipes = [
+        {
+            meats:[
+                {
+                    name: "Tambaqui assado no forno",
+                    img: "/images/tambaqui-assado-no-forno.jpg",
+                    yield: 5,
+                    timeWait: 30,
+                    levelPrice: "preco médio",
+                    ingredients: [
+                        "1 unidade de tambaqui de 3kg",
+                        "4 unidades de limão",
+                        "3 dentes alho amassados",
+                        "1 cebola aberta no meio",
+                        "4 colheres de sopa de azeite",
+                        "Sal a gosto",
+                        "Pimenta à gosto opcional"
+                    ],
+                    preparations: [
+                        "Primeiramente abra a barriga do peixe para limpar bem. Depois faça alguns cortes transversais dos dois lados do peixe. E lave com água e 2 limões",
+
+                        "Coloque uma vasilha e coloque o suco de 2 limão, alho, sal e pimenta à gosto. Deixe marinar por 1 hora no mínimo até 3 horas se preferir. Dessa forma, pega bem tempero.",
+
+                        "Após esse tempo está na hora de assar. Pegue o peixe coloque em um papel alumínio grande, e sobre o peixe passe cebola e o azeite, salpique sal se precisar.",
+
+                        "Agora embrulhe, fechando o papel alumínio com o peixe já temperado e coloque-o em uma travessa de vidro ou assadeira de bolo para poder assar.",
+
+                        "Leve para assar em forno pré aquecido à 190°C por 20 minutos. Após os minutos, retire o papel alumínio e vire o peixe e termine de assar até ficar bem dourado.",
+
+                        "Quando estiver bem dourado estará pronto. Só servir o seu Tambaqui Assado de Forno acompanhado de arroz, salada, purê ou como desejar. Bom Apetite."
+                    ]
+                },
+            ],
+
+            fruits:[]
+        },
+    ];
+
+    return recipes;
+}
