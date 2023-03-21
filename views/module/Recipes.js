df2031dc7b9b485a49b72a5dae08ff9765bf703b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-21 09:43:29 -0300

    create new recipes objc in category meats

--- views/module/Recipes.js
+++ views/module/Recipes.js
@@ -3,11 +3,13 @@ module.exports = function Recipes(){
         {
             meats:[
                 {
-                    name: "Tambaqui assado no forno",
+                    title: "Tambaqui assado no forno",
                     img: "/images/tambaqui-assado-no-forno.jpg",
+                    path:"/categoria/carne/tambaqui-assado-no-forno",
                     yield: 5,
                     timeWait: 30,
                     levelPrice: "preco médio",
+                    rating: 5,
                     ingredients: [
                         "1 unidade de tambaqui de 3kg",
                         "4 unidades de limão",
@@ -31,9 +33,330 @@ module.exports = function Recipes(){
                         "Quando estiver bem dourado estará pronto. Só servir o seu Tambaqui Assado de Forno acompanhado de arroz, salada, purê ou como desejar. Bom Apetite."
                     ]
                 },
-            ],
+                {
+                    title: "Peru de Natal",
+                    img: "/images/Peru-de-Natal.jpg.webp",
+                    path: "",
+                    yield: 5,
+                    timeWait: 2,
+                    levelPrice: "preco médio",
+                    rating: 5,
+                    ingredients: [
+                        "1 peru de 4 a 5 kg",
+                        "1 cebola grande",
+                        "1 lata de cerveja",
+                        "1 colher de chá de açafrão",
+                        "3 dentes de alho",
+                        "4 colheres de sopa de óleo",
+                    ],
+                    preparations: [
+                        "Descongele o peru com um dia de antecedência.",
 
-            fruits:[]
+                        "Tempere o peru, deixando-o marinando por 5 horas. Leve ao forno (180º graus), coberto com papel alumínio por cerca de 2 horas.",
+
+                        "Retire o papel alumínio e aumente a temperatura do forno até corar."
+                    ]  
+                },  
+                {
+                    title: "Filé de Peixe na AirFryer",
+                    img: "/images/receita-de-file-de-tilapia-no-airfryer.webp",
+                    path: "",
+                    yield: 3,
+                    timeWait: 50,
+                    levelPrice: "preco médio",
+                    rating: 3,
+                    ingredients: [
+                        "2 Filés de peixe (sem espinhas)",
+                        "Pimenta do reino a gosto",
+                        "Sal a gosto",
+                        "1 dente de alho",
+                        "1 limão",
+                        "1 cheiro verde",
+                    ],
+                    preparations: [
+                        "Lave os filés de peixe com o limão.",
+
+                        "Amasse o alho, passe nos dois lados dos filés, adicione a pimenta, sal e também o suco de limão.",
+
+                        "Misture em um recipiente para pegar tempero por uns 20 minutos em média.",
+
+                        "Preaqueça a sua airfryer por cerca de 5 minutos.",
+
+                        "Adicione os filés de peixe dentro da cesta.",
+
+                        "Programe a fritadeira para 25 minutos a 180°.",
+
+                        "Vire cada um dos filés a cada 5 minutos para que eles fiquem bem assados.",
+
+                        "Retire da cesta da airfryer e sirva com purê de batata e arroz de preferência."
+                    ]  
+                },  
+                {
+                    title: "Carne moída com molho",
+                    img: "/images/Carne-moída-com-molho.jpg.webp",
+                    path: "",
+                    yield: 8,
+                    timeWait: 30,
+                    levelPrice: "preco médio",
+                    rating: 3,
+                    ingredients: [
+                        "1 kg de carne moída",
+                        "2 colheres de azeite",
+                        "1/2 cebola picada",
+                        "1 tomate",
+                        "1/2 xícara de azeitonas",
+                        "1 Salsinha a gosto",
+                        "1 pitada de sal",
+                        "1 pitada de pimenta-do-reino com cominho",
+                        "2 latas de extrato de tomate"
+                    ],
+                    preparations: [
+                        "Doure a cebola no azeite ou óleo.",
+
+                        "Acrescente a carne moída e refogue.",
+
+                        "Acrescente o tomate, as azeitonas, salsinha, 1 pitada de sal, pimenta-do-reino com cominho, sempre mexendo.",
+
+                        "Acrescente as 2 latas de extrato de tomate e deixe ferver, sempre mexendo.",
+
+                        "Refoge durante 5 minutos para o sabor do molho condesar na carne antes de servir.",
+                    ]  
+                },
+                {
+                    title: "Frango à Brás",
+                    img: "/images/frango-a-bras.jpg",
+                    path: "",
+                    yield: 6,
+                    timeWait: 45,
+                    levelPrice: "preco médio",
+                    rating: 2,
+                    ingredients: [
+                        "2 peitos de frango",
+                        "1 unidade de cebola",
+                        "3 dentes de alho",
+                        "4 unidades de ovos",
+                        "4 colheres de sopa de azeite de oliva",
+                        "1 pacote de batata palha",
+                        "1 punhado de azeitonas",
+                        "2 folhas de louro",
+                        "1 colher de sopa de salsinha picada",
+                        "1 pitada de Pimenta do reino",
+                        "1 pitada de sal"
+                    ],
+                    preparations: [
+                        "Na preparação do Frango à Brás comece por cozer os peitos de frango em água com sal. Para acelerar o processo pode cortá-los em cubos grandes, demorarão cerca de 15 minutos a estar prontos.",
+
+                        "Logo que o frango estiver cozido, o passo seguinte é retirá-lo da água, deixar esfriar e desfiar à mão ou com a ajuda de dois garfos.",
+
+                        "Num tacho ou frigideira grande deixe alourar em azeite o alho picado, a cebola cortada em rodelas e o louro. Este passo é importante para tornar a receita de Frango à Brás mais saborosa!",
+
+                        "Adicione o frango e deixe cozinhar cerca de 5 minutos e tempere. Baixe o fogo, adicione a batata palha e os ovos já batidos ou não.",
+
+                        "Envolta e mexa tudo delicadamente até que os ovos fiquem cozinhados. Quando isso acontecer, retire do fogo sirva seu Frango à Brás decorado com azeitonas, salsinha e pode servir.",
+                    ]  
+                },  
+                {
+                    title: "Peito de frango suculento peruano",
+                    img: "/images/strogonoff-de-carne.webp",
+                    path: "",
+                    yield: 8,
+                    timeWait: 30,
+                    levelPrice: "preco médio",
+                    rating: 3,
+                    ingredients: [
+                        "1 kg de carne de sua preferência, como patinho ou coxão mole",
+                        "3 cebolas médias",
+                        "1 pimentão amarelo",
+                        "1 vidro de champignon ou palmito",
+                        "Sal a gosto",
+                        "Pimenta a gosto",
+                        "3 tomates picados sem pele e sem sementes",
+                        "3 colheres de sopa de óleo de oliva",
+                        "1 cubo de caldo de carne dissolvido em 100 ml de água",
+                    ],
+                    preparations: [
+                        "Em uma panela, adicione o óleo, a carne, a cebola, os tomates, o caldo de carne e deixe cozinhar por 20 minutos.",
+
+                        "Acrescente o ketchup e o champignon e deixe cozinhar até obter um molho consistente e cremoso.",
+
+                        "Desligue o fogo e acrescente o creme de leite sem soro.",
+
+                        "Mexa até incorporar o molho ao creme.",
+
+                        "Coloque em uma forma refratária, decore com tempero, batata palha e sirvar!",
+                    ]  
+                },   
+
+            ],
+            salads:[
+                {
+                    title: "Salada de frutas",
+                    img: "/images/salada-de-frutas.jpg",
+                    path: "",
+                    yield: 5,
+                    timeWait: 10,
+                    levelPrice: "preço econômico",
+                    rating: 5,
+                    ingredients: [
+                        "2 mamões papaia pequenos",
+                        "1 laranja média",
+                        "5 bananas",
+                        "2 maçãs",
+                        "5 morangos maduros",
+                        "1 pêssego",
+                        "10 grãos de uva de sua preferência",
+                        "1 caixa leite condensado (opcional)",
+                        "1/2 colher (sopa) canela em pó"
+                    ],
+                    preparations: [
+                        "Lave e segue todas as frutas sem casca.",
+
+                        "Em seguida pique todos os ingredientes.",
+
+                        "Depois basta colocar tudo em uma tigela funda e acrescentar o leite condensado e a canela em pó, e misturar delicadamente por alguns segundos.",
+
+                        ""
+                    ]  
+                }
+            ],
+            mainCourse:[
+                {
+                    title: "Yakisoba de miojo",
+                    img: "/images/Yakisoba-de-miojo.jpg.webp",
+                    path: "",
+                    yield: 2,
+                    timeWait: 7,
+                    levelPrice: "preço econômico",
+                    rating: 4,
+                    ingredients: [
+                        "1 cebola grande cortada em pedaços grandes",
+                        "1 pimentão grande cortado em rodelas",
+                        "1 cenoura cortada em rodelas finas",
+                        "2 pacotes de miojo tipo talharim",
+                        "2 dentes de alho amassados",
+                        "1/2 maço de brócolis",
+                        "1/2 maço de repolho roxo",
+                        "250 ml de molho shoyu",
+                        "400 g de carne cortada em tirinhas (patinho, mignon ou alcatra) ou frango",
+                        "Sal a gosto"
+                    ],
+                    preparations: [
+                        "Primeiramente, coloque a cenoura e o brócolis em uma panela e cozinhe, adicionando sal a gosto. Ao cozinhar, reserve.",
+
+                        "Agora coloque o miojo para cozinhar também e deixe “al dente”. Coloque para escorrer.",
+
+                        "Refogue o alho e a cebola e deixe reservado. Na mesma panela, frite a carne.",
+
+                        "Quando a carne estiver frita, coloque na panela o alho, a cebola, repolho roxo e pimentão em rodelas.",
+
+                        "Depois de refogados, adicione também a cenoura e o brócolis.",
+
+                        "Agora é o momento de colocar o miojo cozido e também o molho shoyu. Vá misturando.",
+
+                        "Deixe por cerca de 5 minutos em fogo baixo para que o macarrão pegue o sabor do molho shoyu antes de servir.",
+                    ]  
+                },
+                {
+                    title: "Arroz de Sobra",
+                    img: "/images/receita-arroz-de-forno-02.jpg",
+                    path: "",
+                    yield: 5,
+                    timeWait: 30,
+                    levelPrice: "preço econômico",
+                    rating: 3,
+                    ingredients: [
+                        "4 xícaras de chá de arroz cozido",
+                        "100 gramas de muçarela ralada",
+                        "100 gramas de presunto cortado em cubos pequenos",
+                        "1 cenoura ralada",
+                        "2 colheres de sopa de salsa picada",
+                        "2 ovos",
+                        "Meio pote de requeijão",
+                        "3 colheres de sopa de queijo parmesão ralado",
+                        "Pimenta a gosto",
+                        "Sal a gosto"
+                    ],
+                    preparations: [
+                        "Em uma tigela, misture o arroz, a mussarela, o presunto, a cenoura e a salsa.",
+
+                        "Coloque em um refratário untado com margarina.",
+
+                        "No liquidificador, bata os ovos, o leite, o requeijão, o queijo ralado, o sal e a pimenta.",
+
+                        "Despeje sobre o arroz e asse no forno preaquecido a 200 graus Celsius durante 30 minutos ou até dourar.",
+
+                        "Quando o queijo estiver dourado é só tirar e servir essa delícia.",
+                    ]  
+                },
+                {
+                    title: "Arroz Branco com camarão",
+                    img: "/images/Arroz-Branco-com-camarões.jpg.webp",
+                    path: "",
+                    yield: 4,
+                    timeWait: 30,
+                    levelPrice: "preço médio",
+                    rating: 4,
+                    ingredients: [
+                        "1 colher sopa de óleo",
+                        "1 cebola picada",
+                        "1 xícara chá de arroz branco integral",
+                        "2 xícaras chá de água fervente",
+                        "1 colher chá de sal",
+                        "2 colheres sopa de manteiga",
+                        "2 colheres sopa de azeite",
+                        "5 dentes grandes de alho, picados",
+                        "300g de camarão médio limpo",
+                        "Pimenta do reino a gosto",
+                        "2 colheres sopa de cheiro-verde picado"
+                    ],
+                    preparations: [
+                        "Em uma panela, em fogo médio aqueça o óleo e refogue a cebola até dourar.",
+
+                        "Acrescente o arroz, e refogue por alguns minutos. Junte a água fervente ao sal, tampe parcialmente a panela e cozinhe por 10 a 15 minutos ou até a água secar e o arroz ficar cozido.",
+
+                        "Tampe a panela e reserve por 5 minutos.",
+
+                        "Em uma panela grande, aqueça a manteiga e o azeite e doure o alho.",
+
+                        "Junte os camarões e cozinhe em fogo alto, por 5 a 8 minutos, mexendo até ficar macio e rosado.",
+
+                        "Tempere com sal e pimenta. Desligue o fogo, acrescente o arroz reservado e o cheiro-verde e misture delicadamente antes de servir."
+                    ]  
+                },
+                {
+                    title: "Bolinho de arroz quatro queijos",
+                    img: "/images/Bolinho-de-arroz-quatro-queijos.jpg",
+                    path: "",
+                    yield: 5,
+                    timeWait: 20,
+                    levelPrice: "preço médio",
+                    rating: 3,
+                    ingredients: [
+                        "3 xícaras de chá de arroz cozido",
+                        "2 ovos inteiros",
+                        "4 colheres de sopa de queijo parmesão ralado",
+                        "Sal a gosto",
+                        "1/2 xícara de cheiro verde picado",
+                        "1 cebola pequena ralada",
+                        "1 xícara de farinha de trigo",
+                        "100 g de mussarela cortadas em cubinhos",
+                        "4 colheres de sopa de queijo provolone",
+                        "4 colheres de sopa de queijo meia cura",
+                        "4 colheres de sopa de queijo mussarela"
+                    ],
+                    preparations: [
+                        "Misturar em uma tigela todos os ingredientes, menos a mussarela, até formar uma massa.",
+
+                        "Fazer bolinhas dessa massa e abrir ao meio para rechear com cada cubinho de mussarela.",
+
+                        "Depois que fechar a bolinha levar para fritar.",
+
+                        "Seque em um papel toalha.",
+
+                        "Por fim é só servir.",
+                    ]  
+                },
+            ]
         },
     ];
 
