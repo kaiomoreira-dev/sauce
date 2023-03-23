b03190a0ce73fcf668de75da3515172ae5fa1164
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-03-22 21:02:50 -0300

    create loader article, remove zom image script.js

--- views/script/index.js
+++ views/script/index.js
@@ -13,13 +13,17 @@ const iconeShowMenuMobile = document.querySelector(".btnShowMenuMobile");
 
 const navbarMenu = document.querySelector(".navbar-menu")
 
-const main = document.querySelector("main");
+const mainIndex = document.querySelector("#main-1");
 
 const liShowCategories = document.querySelector(".navbar-menu li + li");
 const ulCategories = document.querySelector(".navbar-menu .categories");
 const iconeAngleDown = document.querySelector(".navbar-menu li + li i");
 const header = document.querySelector("header");
 
+// const lastChildArticle = document.getElementById("#loader-article").previousElementSibling;
+const loaderArticle = document.getElementById("#loader-article");
+const loaderArticleElement = document.querySelector("main").lastElementChild;
+const emphasisH2 = document.querySelectorAll("h2");
 
 imagesPost.forEach(image => {
     image.setAttribute("title", "Ler mais")
@@ -61,40 +65,6 @@ iconeShowMenuMobile.addEventListener("click", event =>{
 
 })
 
-// <------- Efeito Zom e Sombra Article Main------->
-// Efeito que adiciona zom e sombra nas imagens principal
-imagesPost.forEach(image => {
-    image.addEventListener("mouseenter", () =>{
-        image.classList.add("zomImageArticleMain");
-    });
-})
-// Efeito que remove zom e sombra nas imagens principal
-imagesPost.forEach(image => {
-    image.addEventListener("mouseleave", () =>{
-        image.classList.remove("zomImageArticleMain");
-    });
-})
-
-// <------- Efeito Zom Article------->
-// Efeito que adiciona zom nas imagens 
-imagesPost.forEach(image => {
-    image.addEventListener("mouseenter", () =>{
-        let imageId = image.getAttribute("id");
-        if(imageId !== "image-1" && imageId !== "image-2"){
-            image.classList.add("zomImageArticle")
-        }
-    })
-})
-// Efeito que remove zom nas imagens
-imagesPost.forEach(image => {
-    image.addEventListener("mouseenter", () =>{
-        let imageId = image.getAttribute("id");
-        if(imageId !== "image-1" && imageId !== "image-2"){
-            image.classList.remove("zomImageArticle")
-        }
-        
-    })
-})
 liShowCategories.addEventListener("click", () =>{
     let liShowCategory = liShowCategories.getAttribute("id");
     if(!liShowCategory){
@@ -106,7 +76,6 @@ liShowCategories.addEventListener("click", () =>{
 });
 liShowCategories.addEventListener("click", () =>{
     let categoriesId = ulCategories.getAttribute("id");
-    console.log(categoriesId)
     if(!categoriesId){
         iconeAngleDown.setAttribute("id","rotateInverse")
         ulCategories.setAttribute("id","showCategories")
@@ -119,7 +88,140 @@ liShowCategories.addEventListener("click", () =>{
     
 })
 
-main.addEventListener("click", () =>{
+mainIndex.addEventListener("click", () =>{
     iconeAngleDown.removeAttribute("id","rotateInverse")
     ulCategories.removeAttribute("id","showCategories")
 })
+
+// lastChildArticle
+// loaderArticle
+
+async function createArticleSecondaryDough(){
+    const recipes = (await import("../module/RecipeTest.js")).default;
+
+    let recipesDough = []
+
+    const emphasis = document.createElement("div");
+    const h2 = document.createElement("h2");
+    emphasis.appendChild(h2);
+    emphasis.classList.add("emphasis")
+    h2.innerHTML = "Massa";
+    mainIndex.insertBefore(emphasis, loaderArticleElement);
+    
+    recipes.forEach(recipe => {
+        for(let dough of recipe.doughs){
+            if(recipesDough.length <= 3){
+                recipesDough.push(dough);
+            }
+        }
+        
+    })
+    
+    const articleSecondaryDough = document.createElement("article")
+
+    for(let dough of recipesDough){
+        const section = document.createElement("section");
+        const figure = document.createElement("figure");
+        const a_link = document.createElement("a");
+        const image = document.createElement("img");
+
+        const divTitle = document.createElement("div");
+        const h1 = document.createElement("h1");
+        const divStars = document.createElement("div");
+
+        articleSecondaryDough.classList.add("article-second-recipe");
+        a_link.setAttribute("href", dough.path)
+        image.setAttribute("src", dough.img)
+        divTitle.classList.add("title");
+        h1.innerHTML = dough.title;
+        divStars.classList.add("stars");
+
+        section.appendChild(figure);
+        figure.appendChild(a_link);
+        a_link.appendChild(image);
+
+        section.appendChild(divTitle);
+        divTitle.appendChild(h1);
+        divTitle.appendChild(divStars);
+
+        for(let i = 0; i < 5; i++){
+            const i_star = document.createElement("i");
+            divStars.appendChild(i_star);
+        }
+
+        articleSecondaryDough.appendChild(section);
+    }
+
+    mainIndex.insertBefore(articleSecondaryDough, loaderArticleElement);
+}
+async function createArticleSecondaryMainCourses(){
+    const recipes = (await import("../module/RecipeTest.js")).default;
+
+    let mainCourses = []
+
+    const emphasis = document.createElement("div");
+    const h2 = document.createElement("h2");
+    emphasis.appendChild(h2);
+    emphasis.classList.add("emphasis")
+    h2.innerHTML = "Prato Principal";
+    mainIndex.insertBefore(emphasis, loaderArticleElement);
+    
+    recipes.forEach(recipe => {
+        for(let course of recipe.mainCourses){
+            if(mainCourses.length <= 3){
+                mainCourses.push(course);
+            }
+        }     
+    })  
+    const articleSecondary = document.createElement("article")
+
+    for(let course of mainCourses){
+        const section = document.createElement("section");
+        const figure = document.createElement("figure");
+        const a_link = document.createElement("a");
+        const image = document.createElement("img");
+
+        const divTitle = document.createElement("div");
+        const h1 = document.createElement("h1");
+        const divStars = document.createElement("div");
+
+        articleSecondary.classList.add("article-second-recipe");
+        a_link.setAttribute("href", course.path)
+        image.setAttribute("src", course.img)
+        divTitle.classList.add("title");
+        h1.innerHTML = course.title;
+        divStars.classList.add("stars");
+
+        section.appendChild(figure);
+        figure.appendChild(a_link);
+        a_link.appendChild(image);
+
+        section.appendChild(divTitle);
+        divTitle.appendChild(h1);
+        divTitle.appendChild(divStars);
+
+        for(let i = 0; i < 5; i++){
+            const i_star = document.createElement("i");
+            divStars.appendChild(i_star);
+        }
+
+        articleSecondary.appendChild(section);
+    }
+
+    mainIndex.insertBefore(articleSecondary, loaderArticleElement);
+}
+
+async function handleInfiniteScroll(){
+    const endOfPage = window.innerHeight + window.pageYOffset >= mainIndex.offsetHeight;
+    if (endOfPage && mainIndex.offsetHeight < 2000) {
+        createArticleSecondaryDough();
+    }
+    if(endOfPage && mainIndex.offsetHeight > 2000) {
+        createArticleSecondaryMainCourses();
+    }
+    if(endOfPage && mainIndex.offsetHeight > 2500 ){
+        window.removeEventListener("scroll", handleInfiniteScroll);
+        loaderArticleElement.remove();
+    }
+  };
+window.addEventListener("scroll", handleInfiniteScroll);
